c7e78a45a1cf6d85db204973dd76cead5df09383
Andre Araujo <araujo@cloudera.com>
2025-06-27 15:13:02 +1000

    Completed version 1.0

--- src/ssb_tools/__init__.py
+++ src/ssb_tools/__init__.py
@@ -1,6 +1,19 @@
-from argparse import ArgumentParser
-from getpass import getpass
+# Copyright (c) 2025, André Araújo
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
+from argparse import ArgumentParser, BooleanOptionalAction
+from getpass import getpass
 import yaml
 
 from ssb_tools.ssb import SsbTools
@@ -32,6 +45,42 @@ def process_arguments(args):
         assert args.job_name or args.job_id or args.all_jobs, (
             "At least one of --job-name or --job-id or --all must be provided.")
 
+    if args.command in ["start-jobs", "update-jobs"]:
+        assert not (args.batch and args.streaming), (
+            "Only one of --batch or --streaming must be provided.")
+        assert not (args.per_job and args.session), (
+            "Only one of --per-job or --session must be provided.")
+
+
+def _add_project_identifier_args(parser):
+    parser.add_argument('-p', '--project-name', action='store', help='Project name.')
+    parser.add_argument('-i', '--project-id', action='store', help='Project ID.')
+
+
+def _add_job_identifier_args(parser):
+    parser.add_argument('-j', '--job-name', action='append',
+                        help='Name of the job to be stopped. This parameter can be used multiple times.')
+    parser.add_argument('-k', '--job-id', action='append',
+                        help='ID of the job to be stopped. This parameter can be used multiple times.')
+
+
+def _add_all_jobs_arg(parser):
+    parser.add_argument('-a', '--all-jobs', action='store_true',
+                        help='Stop all jobs.')
+
+
+def _add_job_update_args(parser):
+    parser.add_argument('--use-savepoint', action=BooleanOptionalAction, default=False,
+                        help='Start the job from the latest savepoint, if specified. Default: Savepoint is not used.')
+    parser.add_argument('--per-job', action='store_true', default=False,
+                        help='Start the job in Per-Job (PROD) mode. Default: What\'s set in the job.')
+    parser.add_argument('--session', action='store_true', default=False,
+                        help='Start the job in SESSION mode. Default: What\'s set in the job.')
+    parser.add_argument('--batch', action='store_true', default=False,
+                        help='Start the job in BATCH mode. Default: Default: What\'s set in the job.')
+    parser.add_argument('--streaming', action='store_true', default=False,
+                        help='Start the job in STREAMING mode. Default: Default: What\'s set in the job.')
+
 
 def main() -> None:
     parser = ArgumentParser()
@@ -40,57 +89,66 @@ def main() -> None:
     parser.add_argument('-u', '--username', action='store', help='SSB username.')
     parser.add_argument('-p', '--password', action='store',
                         help='SSB password. If not specified the password will be prompted (recommended).')
+    parser.add_argument('--debug', action='store_true',
+                        help='Enable debugging output.')
 
     subparsers = parser.add_subparsers(dest='command')
 
     subparser = subparsers.add_parser('list-projects', help='List SSB projects.')
 
     subparser = subparsers.add_parser('list-jobs', help='List SSB jobs')
-    subparser.add_argument('-p', '--project-name', action='store', help='Project name.')
-    subparser.add_argument('-i', '--project-id', action='store', help='Project ID.')
+    _add_project_identifier_args(subparser)
+    _add_job_identifier_args(subparser)
 
     subparser = subparsers.add_parser('list-jobs-state', help='List SSB jobs\' state.')
-    subparser.add_argument('-p', '--project-name', action='store', help='Project name.')
-    subparser.add_argument('-i', '--project-id', action='store', help='Project ID.')
+    _add_project_identifier_args(subparser)
+    _add_job_identifier_args(subparser)
+
+    subparser = subparsers.add_parser('update-jobs', help='Update SSB job properties.')
+    _add_project_identifier_args(subparser)
+    _add_job_identifier_args(subparser)
+    _add_all_jobs_arg(subparser)
+    _add_job_update_args(subparser)
 
     subparser = subparsers.add_parser('stop-jobs', help='Stop SSB jobs.')
-    subparser.add_argument('-p', '--project-name', action='store', help='Project name.')
-    subparser.add_argument('-i', '--project-id', action='store', help='Project ID.')
-    subparser.add_argument('-j', '--job-name', action='append',
-                           help='Name of the job to be stopped. This parameter can be used multiple times.')
-    subparser.add_argument('-k', '--job-id', action='append',
-                           help='ID of the job to be stopped. This parameter can be used multiple times.')
-    subparser.add_argument('-a', '--all-jobs', action='store_true',
-                           help='Stop all jobs.')
+    _add_project_identifier_args(subparser)
+    _add_job_identifier_args(subparser)
+    _add_all_jobs_arg(subparser)
     subparser.add_argument('-s', '--savepoint', action='store_true', default=False,
                            help='Create a savepoint when stopping the job. Default: false.')
 
     subparser = subparsers.add_parser('start-jobs', help='Start SSB jobs.')
-    subparser.add_argument('-p', '--project-name', action='store', help='Project name.')
-    subparser.add_argument('-i', '--project-id', action='store', help='Project ID.')
-    subparser.add_argument('-j', '--job-name', action='append',
-                           help='Name of the job to be stopped. This parameter can be used multiple times.')
-    subparser.add_argument('-k', '--job-id', action='append',
-                           help='ID of the job to be stopped. This parameter can be used multiple times.')
-    subparser.add_argument('-a', '--all-jobs', action='store_true',
-                           help='Stop all jobs.')
+    _add_project_identifier_args(subparser)
+    _add_job_identifier_args(subparser)
+    _add_all_jobs_arg(subparser)
+    _add_job_update_args(subparser)
 
     args = parser.parse_args()
     process_arguments(args)
 
-    ssb = SsbTools(base_url=args.base_url, username=args.username, password=args.password)
+    ssb = SsbTools(base_url=args.base_url, username=args.username, password=args.password, debug=args.debug)
     if args.command == 'list-projects':
         print_json(ssb.list_projects())
     elif args.command == 'list-jobs':
-        print_json(ssb.list_jobs(project_name=args.project_name, project_id=args.project_id))
+        print_json(ssb.list_jobs(project_name=args.project_name, project_id=args.project_id,
+                                 job_names=args.job_name, job_ids=args.job_id))
     elif args.command == 'list-jobs-state':
-        print_json(ssb.list_jobs_state(project_name=args.project_name, project_id=args.project_id))
+        print_json(ssb.list_jobs_state(project_name=args.project_name, project_id=args.project_id,
+                                       job_names=args.job_name, job_ids=args.job_id))
+    elif args.command == 'update-jobs':
+        ssb.update_jobs(project_name=args.project_name, project_id=args.project_id,
+                        job_names=args.job_name, job_ids=args.job_id,
+                        all_jobs=args.all_jobs, use_savepoint=args.use_savepoint,
+                        per_job=args.per_job, session=args.session, batch=args.batch, streaming=args.streaming)
     elif args.command == 'stop-jobs':
-        ssb.stop_jobs(project_name=args.project_name, project_id=args.project_id, job_names=args.job_name,
-                      job_ids=args.job_id, all_jobs=args.all_jobs, savepoint=args.savepoint)
+        ssb.stop_jobs(project_name=args.project_name, project_id=args.project_id,
+                      job_names=args.job_name, job_ids=args.job_id,
+                      all_jobs=args.all_jobs, savepoint=args.savepoint)
     elif args.command == 'start-jobs':
-        ssb.start_jobs(project_name=args.project_name, project_id=args.project_id, job_names=args.job_name,
-                       job_ids=args.job_id, all_jobs=args.all_jobs)
+        ssb.start_jobs(project_name=args.project_name, project_id=args.project_id,
+                       job_names=args.job_name, job_ids=args.job_id,
+                       all_jobs=args.all_jobs, use_savepoint=args.use_savepoint,
+                       per_job=args.per_job, session=args.session, batch=args.batch, streaming=args.streaming)
     else:
         parser.print_help()
         exit(1)
--- src/ssb_tools/ssb.py
+++ src/ssb_tools/ssb.py
@@ -1,32 +1,93 @@
+# Copyright (c) 2025, André Araújo
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
 import requests
+import time
 import urllib3
 
 from ssb_tools.utils import print_json
 
+logging.basicConfig(level=logging.INFO)
+LOG = logging.getLogger(__name__)
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
+def _update_payload(job, sql=None, use_savepoint=None, per_job=False, session=False, batch=False,
+                    streaming=False):
+    payload = {
+        "sql": sql or job['sql'],
+        "mv_endpoints": job['mv_endpoints'],
+        "job_config": {
+            "job_name": job['name'],
+            "autoscaler_config": job['autoscaler_config'],
+            "checkpoint_config": job['checkpoint_config'],
+            "kubernetes_config": job['kubernetes_config'],
+            "mv_config": job['mv_config'],
+            "runtime_config": job['runtime_config'],
+        },
+    }
+    del payload['job_config']['mv_config']['not_indexed_columns']  # this is not a valid option in the rest API
+    if per_job:
+        payload['job_config']["runtime_config"]["execution_mode"] = 'PER_JOB'
+    elif session:
+        payload['job_config']["runtime_config"]["execution_mode"] = 'SESSION'
+    if streaming:
+        payload['job_config']['runtime_config']['runtime_mode'] = 'STREAMING'
+    elif batch:
+        payload['job_config']['runtime_config']['runtime_mode'] = 'BATCH'
+    if use_savepoint is not None:
+        payload['job_config']['runtime_config']['start_with_savepoint'] = use_savepoint
+    return payload
+
+
+def _stop_payload(job=None, savepoint=False):
+    payload = {
+        "savepoint": savepoint or job['runtime_config']['start_with_savepoint'],
+    }
+    return payload
+
+
 class SsbTools(object):
-    def __init__(self, base_url, username, password):
+    def __init__(self, base_url, username, password, debug=False):
         self.base_url = base_url
         self.username = username
         self.password = password
 
         self._session = None
 
+        if debug:
+            LOG.setLevel(logging.DEBUG)
+
     @property
     def session(self):
         if not self._session:
             self._session = requests.Session()
             self._session.auth = (self.username, self.password)
+            self._session.headers.update({
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            })
             self._session.verify = False
         return self._session
 
     def _api_call(self, method, path, expected_status_codes=None, **kwargs):
-        expected_return_codes = expected_status_codes or [requests.codes.ok]
+        expected_status_codes = expected_status_codes or [requests.codes.ok]
         url = self.base_url + path
+        LOG.debug(f'{method} {url}')
         resp = self.session.request(method, url, **kwargs)
-        if resp.status_code not in expected_return_codes:
+        if resp.status_code not in expected_status_codes:
             raise RuntimeError(f"Unexpected response for {method} {url}: {resp}")
         return resp
 
@@ -35,6 +96,9 @@ class SsbTools(object):
 
     def _post(self, path, **kwargs):
         return self._api_call('POST', path, **kwargs)
+
+    def _put(self, path, **kwargs):
+        return self._api_call('PUT', path, **kwargs)
 
     def list_projects(self, project_name=None, project_id=None):
         projects = self._get("/api/v2/projects").json()
@@ -47,11 +111,33 @@ class SsbTools(object):
             project_id = self.list_projects(project_name=project_name)[0]["id"]
         jobs = self._get(f"/api/v2/projects/{project_id}/jobs").json()['jobs']
         return [j for j in jobs
-                if (job_ids is None or j['job_id'] in job_ids) or (job_names is None or j['name'] in job_names)]
+                if (not job_ids or j['job_id'] in job_ids) and (not job_names or j['name'] in job_names)]
 
     def list_jobs_state(self, project_name=None, project_id=None, job_names=None, job_ids=None):
         jobs = self.list_jobs(project_name=project_name, project_id=project_id, job_names=job_names, job_ids=job_ids)
         return [{"job_id": j['job_id'], "job_name": j['name'], "state": j['state']} for j in jobs]
+
+    def _update_job(self, job, use_savepoint=False, per_job=False, session=False, batch=False, streaming=False):
+        payload = _update_payload(job=job, use_savepoint=use_savepoint, per_job=per_job,
+                                  session=session, batch=batch, streaming=streaming)
+        self._put(f"/api/v2/projects/{job['project_id']}/jobs/{job['job_id']}", json=payload)
+
+    def update_jobs(self, project_name=None, project_id=None, job_names=None, job_ids=None, all_jobs=False,
+                    use_savepoint=False, per_job=False, session=False,
+                    batch=False, streaming=False):
+        job_names = job_names or []
+        job_ids = job_ids or []
+        jobs = self.list_jobs(project_name=project_name, project_id=project_id,
+                              job_names=None if all_jobs else job_names,
+                              job_ids=None if all_jobs else job_ids)
+
+        for job in jobs:
+            if job['state'] != "STOPPED":
+                print(f"Job {job['name']} (job_id={job['job_id']}) is already in state {job['state']}.")
+            else:
+                print(f"Updating job {job['name']} (job_id={job['job_id']})")
+                self._update_job(job, use_savepoint=use_savepoint, per_job=per_job, session=session, batch=batch,
+                                 streaming=streaming)
 
     def stop_jobs(self, project_name=None, project_id=None, job_names=None, job_ids=None, all_jobs=False,
                   savepoint=False):
@@ -66,38 +152,51 @@ class SsbTools(object):
             else:
                 print(f"Stopping job {job['name']} (job_id={job['job_id']})")
                 print(self._post(f"/api/v2/projects/{job['project_id']}/jobs/{job['job_id']}/stop",
-                                 json={
-                                     'savepoint': savepoint,
-                                 }).text)
+                                 json=_stop_payload(job=job, savepoint=savepoint)).text)
 
-    def _start_payload(self, job=None, sql=None, execution_mode=None, runtime_mode=None):
-        payload = {
-            "sql": sql or job['sql'],
-            # "selection": False,
-            "job_config": {
-                "job_name": job['name'],
-                "runtime_config": {
-                    "execution_mode": execution_mode or job['runtime_config']['execution_mode'],
-                }
-            }
-        }
-        if runtime_mode or 'runtime_mode' in job['runtime_config']:
-            payload['job_config']['runtime_config']['runtime_mode'] = runtime_mode \
-                                                                      or job['runtime_config']['runtime_mode']
-        return payload
-
-    def start_jobs(self, project_name=None, project_id=None, job_names=None, job_ids=None, all_jobs=False):
+    def start_jobs(self, project_name=None, project_id=None, job_names=None, job_ids=None, all_jobs=False,
+                   use_savepoint=False, per_job=False, session=False, batch=False, streaming=False):
         job_names = job_names or []
         job_ids = job_ids or []
-
         jobs = self.list_jobs(project_name=project_name, project_id=project_id,
                               job_names=None if all_jobs else job_names,
                               job_ids=None if all_jobs else job_ids)
+        # TODO: Make multiple job starts asynchronous
         for job in jobs:
             if job['state'] != "STOPPED":
                 print(f"Job {job['name']} (job_id={job['job_id']}) is already in state {job['state']}.")
             else:
                 print(f"Starting job {job['name']} (job_id={job['job_id']})")
-                print_json(self._post(f"/api/v2/projects/{job['project_id']}/jobs/{job['job_id']}/execute",
-                                      json=self._start_payload(job)).json())
-
+                self._update_job(job, use_savepoint=use_savepoint, per_job=per_job, session=session, batch=batch,
+                                 streaming=streaming)
+                resp = self._post(f"/api/v2/projects/{job['project_id']}/jobs/{job['job_id']}/execute",
+                                  expected_status_codes=[200, 500])
+                if resp.status_code == 200:
+                    print(resp.json())
+                else:
+                    # TODO: Remove this once the API is improved
+                    # Currently the execute call can timeout if the jobs takes a bit too long to start, which
+                    # is common for PROD jobs. In this case we check for job status to ensure it started ok
+                    attempts = 120
+                    job_state = 'STOPPED'
+                    while attempts > 0 and job_state in ["STOPPED", "INITIALIZING"]:
+                        job = self.list_jobs(project_id=job['project_id'], job_ids=[job['job_id']])[0]
+                        job_state = job['state']
+                        LOG.debug(f"Job {job['name']} (job_id={job['job_id']}) is in state {job_state}.")
+                        if job_state == "STOPPED":
+                            attempts -= 1
+                            time.sleep(1)
+                    if job_state == "RUNNING":
+                        print_json({
+                            "responses": [
+                                {
+                                    "type": "job",
+                                    "ssb_job_id": job['job_id'],
+                                    "job_name": job['name'],
+                                    "flink_job_id": job['flink_job_id'],
+                                    "sample_id": job['sample_id'],
+                                }
+                            ]
+                        })
+                    else:
+                        raise RuntimeError(f"Job {job['name']} (job_id={job['job_id']}) failed to start.")
